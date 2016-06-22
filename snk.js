46afbb6d1ae73561b8236d8214288d8288b6d375
HeathFeath49 <hstarkie@lions.molloy.edu>
2016-06-22 09:28:30 -0400

    inputCheck and snake movement

--- snk.js
+++ snk.js
@@ -2,13 +2,15 @@
 
 $('document').ready(function(){
 
-	//canvas information
+	//canvas variables
 	var canvas = $('#canvas')[0];
 	var ctx = canvas.getContext('2d');
 	var canWidth = $('#canvas').width();
 	var canHeight = $('#canvas').height();
+	//game variables
 	var cellSize = 20;
 	var snakeArr = [];
+	var direction = "right";
 
 	function initiateGame(){
 		createSnake();
@@ -16,7 +18,7 @@ $('document').ready(function(){
 		if(typeof game_loop != "undefined"){
 			clearInterval(game_loop);
 		}
-		game_loop = setInterval(draw, 60);
+		game_loop = setInterval(draw, 150);
 
 	}
 
@@ -29,6 +31,7 @@ $('document').ready(function(){
 		for(var p=snakeLength-1;p>=0;p--){
 			snakeArr.push({xVal:p,yVal:0});
 		}
+		/*console.log(snakeArr.length);*/
 	}
 
 	//draws snake and the background to screen
@@ -44,6 +47,8 @@ $('document').ready(function(){
 			var currentCell = snakeArr[b];
 			drawCell(currentCell.xVal,currentCell.yVal); 
 		}
+		checkInput();
+		moveSnake();
 
 	}
 	//function to draw individual cells
@@ -52,6 +57,52 @@ $('document').ready(function(){
 		ctx.fillRect(x*cellSize,y*cellSize,cellSize,cellSize);
 		ctx.strokeStyle = 'white';
 		ctx.strokeRect(x*cellSize,y*cellSize,cellSize,cellSize);
+	}
+
+	//function to check player input
+	function checkInput(){
+		$(document).on('keypress',function(keyNum){
+			switch(keyNum.which){
+				case 119:
+					direction = 'up';
+					break;
+				case 97:
+					direction = 'left';
+					break;
+				case 115:
+					direction = 'down';
+					break;
+				case 100:
+					direction = 'right';
+					break;
+
+			}
+		})
+	}
+
+	//function to move snake
+	function moveSnake(){
+		var sTx = snakeArr[0].xVal;
+		var sTy = snakeArr[0].yVal;
+		var tail;
+
+		//check user input
+		if(direction == "right"){
+			sTx++;
+		}
+		else if(direction == "left"){
+			sTx--;
+		}
+		else if(direction == "up"){
+			sTy--;
+		}
+		else if(direction == "down"){
+			sTy++;
+		}
+
+		tail = snakeArr.pop();
+		tail.xVal = sTx; tail.yVal = sTy;
+		snakeArr.unshift(tail);
 	}
 
 });
